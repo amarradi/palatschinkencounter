414c973a2d65d3191dfd3286ba7243b44c777e73
Marcus Radisch <radischm@gmail.com>
2021-08-29 18:42:51 +0200

    counter reset item options menu

--- app/src/main/java/com/git/amarradi/palatschinkencounter/MainActivity.java
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/MainActivity.java
@@ -1,12 +1,17 @@
 package com.git.amarradi.palatschinkencounter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -40,6 +45,28 @@ public class MainActivity extends AppCompatActivity {
         load_data();
         updateViews();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_clean:
+                reset_counter();
+
+                Toast.makeText(this, "reset counter", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void save_data() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -55,6 +82,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateViews() {
         textView.setText(Integer.toString(savedCounter));
+    }
+
+    public void reset_counter() {
+        counter = 0;
+        save_data();
+        updateViews();
     }
 
 }
