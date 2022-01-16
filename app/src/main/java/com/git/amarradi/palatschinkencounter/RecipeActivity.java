814b20aee2cd62cc3fe4a07167ea4789e2af2652
Marcus Radisch <radischm@gmail.com>
2022-01-16 21:11:51 +0100

    bugfix recipe

---
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
@@ -0,0 +1,16 @@
+package com.git.amarradi.palatschinkencounter;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+
+    }
+}
