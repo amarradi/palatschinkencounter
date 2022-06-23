36f6f68224341ed09e27657a4c3768603a2ee190
Marcus Radisch <radischm@gmail.com>
2022-06-23 11:51:33 +0200

    Palatschinkencounter 1.0.7 SettingsFragment preferences.xml implementiert Rezept Style Counterreset überarbeitet

--- app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
@@ -8,23 +8,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecipeActivity extends AppCompatActivity {
-    ListView listView_Ingredients;
+    ListView listView_Recipe;
 
-    String[] listItem_ingredients;
+    String[] listItem_Recipe;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
-       listView_Ingredients = findViewById(R.id.listViewIngredients);
-       listItem_ingredients = getResources().getStringArray(R.array.ingredients_array);
 
-       
+        listView_Recipe = findViewById(R.id.listViewIngredients);
+        listItem_Recipe = getResources().getStringArray(R.array.ingredients_array);
 
-       final ArrayAdapter<String> arrayAdapter_Ingredients = new ArrayAdapter<>(this,
-                R.layout.simple_list_item_1, listItem_ingredients);
-       listView_Ingredients.setAdapter(arrayAdapter_Ingredients);
+
+
+        final ArrayAdapter<String> arrayAdapter_Ingredients = new ArrayAdapter<>(this,
+                R.layout.simple_list_item_1, listItem_Recipe);
+        listView_Recipe.setAdapter(arrayAdapter_Ingredients);
+
 
     }
 }
