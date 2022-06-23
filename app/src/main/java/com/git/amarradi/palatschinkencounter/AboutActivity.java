01ada9b80c1ea8548cfc1c6145e407b2be8e4c00
Marcus Radisch <radischm@gmail.com>
2022-06-23 10:44:34 +0200

    Palatschinkencounter 1.0.7 SettingsFragment preferences.xml implementiert Rezept Style Counterreset überarbeitet

--- app/src/main/java/com/git/amarradi/palatschinkencounter/AboutActivity.java
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/AboutActivity.java
@@ -2,17 +2,14 @@ package com.git.amarradi.palatschinkencounter;
 
 import android.annotation.SuppressLint;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +19,15 @@ public class AboutActivity extends AppCompatActivity {
         TextView havefun = findViewById(R.id.tvhavefun);
         TextView oss = findViewById(R.id.itsos);
         TextView appVersion = findViewById(R.id.tvVersion);
-        Typeface typeface = getResources().getFont(R.font.opensans_regular);
-        appVersion.setTypeface(typeface);
-        title.setTypeface(typeface);
-        whatsdo.setTypeface(typeface);
-        havefun.setTypeface(typeface);
-        oss.setTypeface(typeface);
+        Typeface typeface_regular = getResources().getFont(R.font.opensans_regular);
+        Typeface typeface_bold = getResources().getFont(R.font.opensans_extrabold);
+        appVersion.setTypeface(typeface_regular);
+        title.setTypeface(typeface_regular);
+        whatsdo.setTypeface(typeface_regular);
+        havefun.setTypeface(typeface_bold);
+        oss.setTypeface(typeface_regular);
+        TextView resethow = findViewById(R.id.resethowto);
+        resethow.setTypeface(typeface_regular);
         //Resources resources = getResources();
         @SuppressLint({"StringFormatInvalid", "LocalSuppress"}) String version = String.format(getResources().getString(R.string.version), BuildConfig.VERSION_NAME);
         appVersion.setText(version);
--- app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
@@ -1,48 +1,30 @@
 package com.git.amarradi.palatschinkencounter;
 
-import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RecipeActivity extends AppCompatActivity {
+    ListView listView_Ingredients;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    String[] listItem_ingredients;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+       listView_Ingredients = findViewById(R.id.listViewIngredients);
+       listItem_ingredients = getResources().getStringArray(R.array.ingredients_array);
 
-        TextView recipe = findViewById(R.id.tv_recipe_txt);
-        TextView ingredients = findViewById(R.id.pancake_ingredients);
-        TextView flour = findViewById(R.id.tv_flour);
-        TextView eggs = findViewById(R.id.tv_eggs);
-        TextView milk = findViewById(R.id.tv_milk);
-        TextView mineralWater = findViewById(R.id.tv_mineral_water);
-        TextView salt = findViewById(R.id.tv_salt);
-        TextView preparation = findViewById(R.id.pancake_preparation);
-        TextView deliuteEggs = findViewById(R.id.tv_dilute_eggs);
-        TextView addFlour = findViewById(R.id.tv_add_flour);
-        TextView addMilk = findViewById(R.id.tv_add_oil);
-        TextView addOil = findViewById(R.id.tv_add_mineral_water);
+       
 
-        Typeface typeface = getResources().getFont(R.font.opensans_regular);
+       final ArrayAdapter<String> arrayAdapter_Ingredients = new ArrayAdapter<>(this,
+                R.layout.simple_list_item_1, listItem_ingredients);
+       listView_Ingredients.setAdapter(arrayAdapter_Ingredients);
 
-        recipe.setTypeface(typeface);
-        ingredients.setTypeface(typeface);
-        flour.setTypeface(typeface);
-        eggs.setTypeface(typeface);
-        milk.setTypeface(typeface);
-        mineralWater.setTypeface(typeface);
-        salt.setTypeface(typeface);
-        preparation.setTypeface(typeface);
-        deliuteEggs.setTypeface(typeface);
-        addFlour.setTypeface(typeface);
-        addMilk.setTypeface(typeface);
-        addOil.setTypeface(typeface);
     }
 }
