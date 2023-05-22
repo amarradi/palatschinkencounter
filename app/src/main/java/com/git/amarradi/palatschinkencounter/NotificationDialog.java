a3ab4786a4bf4bc8b5b41c58a0f272afa244a111
marcus <radischm@gmail.com>
2023-05-22 11:54:26 +0200

    some changes

---
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/NotificationDialog.java
@@ -0,0 +1,47 @@
+package com.git.amarradi.palatschinkencounter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class NotificationDialog extends AppCompatDialogFragment {
+    private NotificationDialogListener listener;
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity(), R.style.MyAlertDialogTheme);
+        builder.setTitle(getResources().getString(R.string.notification))
+                .setIcon(R.drawable.logo_psc_512_transparent)
+                .setMessage(getResources().getString(R.string.portionstoosmall))
+                .setNeutralButton(getResources().getString(R.string.OK),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            listener = (NotificationDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString());
+        }
+    }
+
+    public interface NotificationDialogListener {
+        void onOKClicked();
+    }
+
+}
--- app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
+++ app/src/main/java/com/git/amarradi/palatschinkencounter/RecipeActivity.java
@@ -2,10 +2,16 @@ package com.git.amarradi.palatschinkencounter;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,37 +23,87 @@ import com.git.amarradi.palatschinkencounter.models.RecipeModel;
 
 import java.util.ArrayList;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements NotificationDialog.NotificationDialogListener{
 
     ArrayList<RecipeModel> ingredientsModels = new ArrayList<>();
     ArrayList<RecipeModel> preparationModels = new ArrayList<>();
 
-    @SuppressLint("ResourceAsColor")
+    private int portions = 1;
+
+    @SuppressLint({"ResourceAsColor", "StringFormatInvalid", "NotifyDataSetChanged"})
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         setTheme(R.style.Theme_Palatschinkencounter);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        TextView ingredients;
+
+        ImageButton decrease;
+
+        ImageButton increase;
+
+
+        final int[] person = {getResources().getInteger(R.integer.portions)};
+        ingredients = findViewById(R.id.ingredients_tv);
+        Typeface typeface_regular = getResources().getFont(R.font.opensans_regular);
+        Typeface typeface_bold = getResources().getFont(R.font.opensans_bold);
+        TextView recipe_head = findViewById(R.id.recipe_head_tv);
+        recipe_head.setTypeface(typeface_bold);
+
+        ingredients.setTypeface(typeface_regular);
+        ingredients.setText(String.format(getResources().getString(R.string.pancake_ingredients), person[0]));
 
         RecyclerView recyclerView_Ingredients = findViewById(R.id.recyclerView_ingredients);
 
-        setupRecipeModels();
+        setupRecipeModels(portions);
 
         RecylerViewRecipeAdapter recylerViewIngredientsAdapter =
                 new RecylerViewRecipeAdapter(this, ingredientsModels);
         recyclerView_Ingredients.setAdapter(recylerViewIngredientsAdapter);
         recyclerView_Ingredients.setLayoutManager(new LinearLayoutManager(this));
-
-
         RecyclerView recyclerView_Preparation = findViewById(R.id.RecyclerView_preparation);
 
         setupPreparationModels();
+
         RecylerViewRecipeAdapter recylerViewPreparationAdapter =
                 new RecylerViewRecipeAdapter(this, preparationModels);
         recyclerView_Preparation.setNestedScrollingEnabled(false);
         recyclerView_Preparation.setAdapter(recylerViewPreparationAdapter);
         recyclerView_Preparation.setLayoutManager(new LinearLayoutManager(this));
+
+        increase = findViewById(R.id.increase);
+        increase.setOnClickListener(v -> {
+            portions++;
+            ingredients.setText(String.format(getResources().getString(R.string.pancake_ingredients), portions));
+            Log.d("setOnClickListener","value of persons: "+ portions);
+
+            ingredientsModels.clear();
+            recylerViewIngredientsAdapter.notifyDataSetChanged();
+            setupRecipeModels(portions);
+
+        });
+
+        decrease = findViewById(R.id.decrease);
+
+        decrease.setOnClickListener(v -> {
+            if(portions>=1){
+                portions--;
+                ingredients.setText(String.format(getResources().getString(R.string.pancake_ingredients), portions));
+                ingredientsModels.clear();
+                recylerViewIngredientsAdapter.notifyDataSetChanged();
+                setupRecipeModels(portions);
+            } else{
+                ingredients.setText(String.format(getResources().getString(R.string.pancake_ingredients), portions));
+
+                NotificationDialog notificationDialog = new NotificationDialog();
+                notificationDialog.show(getSupportFragmentManager(),getResources().getString(R.string.notification));
+            }
+        });
+
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,18 +122,32 @@ public class RecipeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void setupRecipeModels(int person) {
+        Log.d("setupRecipeModels:", "value of person " + person);
+        Resources resources = getResources();
+
+        int[] ingredients = resources.getIntArray(R.array.Ingredients);
+        String[] strings_ingredients = resources.getStringArray(R.array.ingredients_array);
+        int count = 0;
+        for (String strings_ingredient : strings_ingredients) {
+            String s = String.format(strings_ingredient, ingredients[count]*person);
+            count++;
+            ingredientsModels.add(new RecipeModel(s));
+            }
+    }
+
+
     private void setupPreparationModels() {
+
         String[] strings_preparation = getResources().getStringArray(R.array.preparation_array);
         for (String s : strings_preparation) {
             preparationModels.add(new RecipeModel(s));
         }
     }
 
-    public void setupRecipeModels() {
-        String[] strings_ingredients = getResources().getStringArray(R.array.ingredients_array);
-        for (String strings_ingredient : strings_ingredients) {
-            ingredientsModels.add(new RecipeModel(strings_ingredient));
-        }
-    }
 
+    @Override
+    public void onOKClicked() {
+
+    }
 }
